d5fca334ca0e9cd02680da6281f71a3fb09525fc
Leandro Gomez <leanngomezzz05@gmail.com>
2026-07-07 01:45:22 -0300

    refactor: reubicacion y agregado de nueva ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { ListaProductosComponent } from './components/lista-productos/lista-productos';
 import { CarritoComponent } from './components/carrito/carrito';
 import { HistorialPedidosComponent } from './components/historial-pedidos/historial-pedidos';
+import { LoginComponent } from './components/login/login';
+
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'productos', component: ListaProductosComponent },
   { path: 'carrito', component: CarritoComponent },
   { path: 'pedidos', component: HistorialPedidosComponent },
-  { path: '', redirectTo: 'productos', pathMatch: 'full' }, // Ruta por defecto
-  { path: '**', redirectTo: 'productos' } // Si escriben cualquier verdura, van a productos
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Ruta por defecto
+  { path: '**', redirectTo: '/login' } // Si escriben cualquier verdura, van a productos
 ];
