da23895a7195c4db9b091f0cee5c8beab9417f05
Leandro Gomez <leanngomezzz05@gmail.com>
2026-07-07 02:11:07 -0300

    fix: agregue guards a las rutas correspondientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,24 @@ import { ListaProductosComponent } from './components/lista-productos/lista-prod
 import { CarritoComponent } from './components/carrito/carrito';
 import { HistorialPedidosComponent } from './components/historial-pedidos/historial-pedidos';
 import { LoginComponent } from './components/login/login';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'productos', component: ListaProductosComponent },
-  { path: 'carrito', component: CarritoComponent },
-  { path: 'pedidos', component: HistorialPedidosComponent },
+
+  //Rutas protegidas para cualquier usuario logueado
+  { path: 'productos', component: ListaProductosComponent, canActivate: [authGuard]},
+  { path: 'carrito', component: CarritoComponent, canActivate: [authGuard]},
+  { path: 'pedidos', component: HistorialPedidosComponent, canActivate: [authGuard]},
+
+  //Ruta super protegida para ADMIN
+  { 
+    path: 'admin', 
+    component: AdminDashboardComponent, 
+    canActivate: [authGuard], 
+    data: { rol: 'ADMIN' } 
+  },
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Ruta por defecto
   { path: '**', redirectTo: '/login' } // Si escriben cualquier verdura, van a productos
 ];
